4215ce9093b648ff106199811933f420a0f71d74
Hilary <hilary.scott90@gmail.com>
2019-07-13 13:13:22 -0400

    create footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Idiom from "./components/Idiom/Idiom";
+import Footer from "./components/Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84539/preview.svg" className="App-logo" alt="logo" />
+          <h2>GoBananas</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Idiom></Idiom>
+        <Footer></Footer>
       </div>
     );
   }
--- client/src/components/Idiom/Idiom.js
+++ client/src/components/Idiom/Idiom.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./idiom.css";
+import "./Idiom.css";
 
 function Idiom(props) {
     return (
         <div className="container">
             <div className="idiomCard">
-                <div id={props.id}></div>
+                <div id={props.id}>
+                    <p>Test content render!</p></div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Idiom; 
