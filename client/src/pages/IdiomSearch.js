344517a8ed6a3a622e1414bb47e85d46cce3f546
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-14 11:29:08 -0400

    login page

--- client/src/pages/IdiomSearch.js
+++ client/src/pages/IdiomSearch.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 
-function Login() {
+function idiomSearch() {
   return (
     <div>
             <p>
@@ -15,4 +15,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default idiomSearch;
