a33d103fed600836b512de5bacc7c448ad326a6d
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-16 17:18:12 -0400

    login & signup components

---
+++ client/src/components/Login/login.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+
+class LoginForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: '',
+            redirectTo: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log('handleSubmit')
+
+        axios.post('user/login', {
+            usernmae: this.state.username,
+            password: this.state.password
+        })
+            .then(response => {
+                console.log('login response: ')
+                console.log('response')
+                if (response.status === 200) {
+                    this.props.updateUser({
+                        loggedIn: true,
+                        username: response.data.username
+                    })
+                    this.setState({
+                        redirectTo: '/'
+                    })
+                }
+            }).catch(error => {
+                console.log('login error: ')
+                console.log('error')
+            })
+    }
+
+
+    render() {
+        if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } else {
+            // can change this to look however it needs to look
+            return (
+                <div>
+                    <form>
+                        <div>
+                            <div className="login-form">
+                                <label className="form-label" for="username">Username</label>
+                                <input className="form-input"
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    placeholder="Username"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="login-form">
+                                <label className="form-label" for="password">Password</label>
+                                <input className="form-input"
+                                    name="username"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn"
+                                onClick={this.handleSubmit}
+                                type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+            )
+        }
+    }
+}
+
+export default LoginForm
