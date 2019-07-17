1491aa528a3cebd93e96ea2c1b736411e381e0ec
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-17 10:43:35 -0400

    utils

---
+++ client/src/utils/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getIdioms: function() {
+    return axios.get("/api/idioms");
+  },
+  // Gets the book with the given id
+  getIdiom: function(id) {
+    return axios.get("/api/idioms/" + id);
+  },
+  // Deletes the book with the given id
+  deleteIdiom: function(id) {
+    return axios.delete("/api/idioms/" + id);
+  },
+  // Saves a book to the database
+  saveIdiom: function(bookData) {
+    return axios.post("/api/idioms", bookData);
+  }
+};
