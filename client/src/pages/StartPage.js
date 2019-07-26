162f8107d502d1756e87a3183cbe6963795b999f
Maya Chen <fangnihuang2016@gmail.com>
2019-07-26 17:08:19 -0400

    startPage card change

--- client/src/pages/StartPage.js
+++ client/src/pages/StartPage.js
@@ -7,18 +7,15 @@ class StartPage extends Component {
     <div className ="StartPage">
         <BananaLogo/>
         <div className="card text-center">
-        <div className="card-header">
-        Welcome to GoBananas...
-         </div>
-  <div className="card-body">
-    <h5 className="card-title"></h5>
-    <p className="card-text">It's time to search for English idioms and save your favourite ones.</p>
-    <a href="#" className="btn text-white bg-warning">Create your account !</a>
-  </div>
-  <div className="card-footer text-muted">
-  Already have an account? <a href="/" className="text-dark">Login here</a>
-  </div>
-</div>
+       <div >
+       <h5 className="card-title">Welcome to GoBananas...</h5>
+       <p className="text-secondary">Search for English idioms and save your favourite ones.</p>
+       <a href="#" className="btn text-white bg-warning">Create your account !</a>
+       <p></p>
+        </div>
+       <hr></hr>
+       Already have an account? <a href="/" className="text-dark">Login here</a>     
+        </div>
    </div>
   );
 }
