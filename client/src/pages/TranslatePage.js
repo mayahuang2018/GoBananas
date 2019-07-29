11385949a59e09332f57be0b8efddbed45482779
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-28 23:10:54 -0400

    translate page

--- client/src/pages/TranslatePage.js
+++ client/src/pages/TranslatePage.js
@@ -8,51 +8,52 @@ import LanguageButton from "../components/LanguageButton"
 import TranslatedDescription from "../components/TranslatedDescription";
 import BananaLogo from "../components/BananaLogo";
 import OriginalIdiom from "../components/OriginalIdiom";
-import languages from "../../../server/scripts/languageSeeds.js/index.js"
 
 class TranslatePage extends Component {
 
-  renderDropDown = () => {
-    let lanugages = {
-      array: []
-    };
+state = {
+  buttonLanguages: [], //language to populate the buttons
+  clicked: false, //if state clicked = true then will do axios to MS translate with this language
+  // languageCodePicked: this.state.languageCodePicked, //which language code to use to send to MS translate
+}
 
-    for (var i=0)
-  }
+componentDidMount() {
+  API.getLanguagesAll(this.state.language, this.state.languageCode)
+  .then(res => {
+    console.log("got langauges from api");
+    const buttonLanguages = res.data;
+    this.setState({ buttonLanguages });
+  })
+};
 
-  //  loadLanguages = () =>{
-  //   console.log(this.state); 
-  //   API.getlanguages()
-  //    .then(res=>
-  //     this.setState({languages: res.data, languageCode: "", language: ""})
-      
-  //     ).catch(err => console.log(err));
-   
-  //   }
+handleClick = (event, id) => {
+  event.preventDefault();
+  console.log("clicked");
+  let pickedLanguage = this.state.languages.find(i => i.id === id)
 
+  if (pickedLanguage.clicked === true) {
+    // console.log(this)
 
-
-
-  handleClick = (event) => {
-    event.preventDefault();
-    console.log("This button was clicked.");
-    console.log({languages: this.state.languages, languageCode: this.state.languageCode})
-    // rest of my function
-    // Click button =
-    // 1. take idion choose
-    // 2. tkae the langauge code
+    // 1. take idiom choosen
+    // 2. take the langauge code
     // 3. send to MS api for translate - axios call
     // 4. show/display the translated idiom
-// 
+  }
+}
+
+// languageCodePicked = () => {
+//  fetch("../utils/languagesAPI.js")
+//  .then(response => response.json)
+//  .then(languages => console.log(JSON.stringify(languages)))
+//  .catch(err => console.log(err))
+// }
+
     
-    
-    // this.setState({languageCode: value});
-    // console.log(this.setState({languageCode: value}))
-  };
 
   render() {
     return (
       <div className="TranslatePage">
+       
         <NavBar />
         <BananaLogo />
         <OriginalIdiom />
@@ -63,6 +64,7 @@ class TranslatePage extends Component {
         <TranslatedDescription
           value={this.state.translatedIdiom} />
         <Footer />
+        
       </div>
     );
   }
