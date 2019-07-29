0077f76b76a1a6eeac09695623ef9cc882b51242
Maya Chen <fangnihuang2016@gmail.com>
2019-07-28 21:29:51 -0400

    saved idiom cards css

---
+++ client/src/components/Savedidiom/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Savedidiom.css"
+
+
+function SavedIdiomCard(props) {
+    return (
+            <div className="SavedIdiom">
+
+<div className="front">Idiom</div>
+<div className="back">Explaination</div>
+                
+            </div>    
+                );
+            }
+            
+    export default SavedIdiomCard; 
