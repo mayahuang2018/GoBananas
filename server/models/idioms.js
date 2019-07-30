b8a1c6698e66e72b945982f347cf90830bfc55ed
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-30 15:30:41 -0400

    idiomsController and seeds

--- server/models/idioms.js
+++ server/models/idioms.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const idiomsSchema = new Schema({
     idiom: { type: String, required: true, trim: true },
+    meaning: { type: String, required: true, trim: true},
+    meaning: { type: String, required: true, trim: true}
 });
 
 const Idioms = mongoose.model("Idioms", idiomsSchema);
