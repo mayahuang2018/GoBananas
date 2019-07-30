aac61c3ed8fbb8948c7c2411d858c4ea815e3f53
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-29 23:34:43 -0400

    server app.js

--- client/src/components/LanguageButton/index.js
+++ client/src/components/LanguageButton/index.js
@@ -7,34 +7,60 @@ import 'react-dropdown/style.css';
 // import LanugageChoice from "./LanguageChoice"
 
 
-class LanguageButton extends Component {
+function LanguageButton(props) {
 
-    state = {
-        buttonLanguagesArray: [],
+    const buttonLanguagesArray = [];
+
+    const buttonLanguages = () => {
+    API.getLanguagesAll()
+    .then(res => buttonLanguagesArray.push(res))
+    console.log(buttonLanguages)
     }
+    console.log(buttonLanguagesArray)
 
-    componentDidMount() {
-        API.getLanguagesAll()
-            .then(res => {
-                const buttonLanguagesArray = res.data;
-                this.setState({ buttonLanguagesArray })
-            })
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Language
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    { this.state.buttonLanguagesArray.map((buttonLanguage, index) => <div className="dropdown-item" type="button" key={index}>{ buttonLanguage.language }</div>)}
-                    </div>       
+    return (
+        <div>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Language
+                     </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    {buttonLanguagesArray.map((buttonLanguage, index) => <div className="dropdown-item" type="button" key={index}> {buttonLanguage} </div>)}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
+// class LanguageButton extends Component {
+
+//     state = {
+//         buttonLanguagesArray: [],
+//     }
+
+//     componentDidMount() {
+//         API.getLanguagesAll()
+//             .then(res => {
+//                 const buttonLanguagesArray = res.data;
+//                 this.setState({ buttonLanguagesArray })
+//             })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className="dropdown">
+//                     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                         Language
+//                     </button>
+//                     <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+//                     { this.state.buttonLanguagesArray.map((buttonLanguage, index) => <div className="dropdown-item" type="button" key={index}> { this.state.buttonLanguage} </div>)}
+//                     </div>       
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default LanguageButton;
