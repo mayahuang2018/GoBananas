4aa9749c80e24ffd3d458575a8451ce03a3b850d
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-08-03 12:58:02 -0400

    seeds

--- server/scripts/idiomSeeds.js
+++ server/scripts/idiomSeeds.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/goBananas", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_t906tgsp:pr9oig0jc05d6lcd9br4b6i3pf@ds113169.mlab.com:13169/heroku_t906tgsp", { useNewUrlParser: true });
   // "mongodb://heroku_t906tgsp:pr9oig0jc05d6lcd9br4b6i3pf@ds113169.mlab.com:13169/heroku_t906tgsp"
 
 mongoose.set('useNewUrlParser', true);
