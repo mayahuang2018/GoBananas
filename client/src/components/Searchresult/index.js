2afdc665382b75af3b35e7dd9b5426f220ad4148
Maya Chen <fangnihuang2016@gmail.com>
2019-08-06 19:45:59 -0400

    accept incoming change

--- client/src/components/Searchresult/index.js
+++ client/src/components/Searchresult/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
- import React, {Component} from "react";
- import propTypes from "prop-types"
- import "./Searchresult.css"
- import LanguageButton from"../LanguageButton"
- import LikeButton from "../LikeButton"
- import TranslatedDescription from "../TranslatedDescription"
- 
- export default class SearchResult extends Component{
- 
-     static propTypes ={
-         searchIdiom: propTypes.string.isRequired
-     }
- 
-     state = {
-         initView:true,
-         loading:false,
-         idioms:null,
-         errorMasg:null
-     }
-
-     //call back when the component receive a new idiom
-     componentWillReceiveProps (newProps){
-         //debugger;
-         console.log(newProps);
- 
-=======
 
 import React, { Component } from "react";
 import propTypes from "prop-types"
@@ -60,52 +33,12 @@ export default class SearchResult extends Component {
         //debugger;
         console.log(newProps);
 
->>>>>>> 05ed957e50f6e081d43011ecb3d32a59eb06b5ad
         //set loading...
         this.setState({
             initView: false,
             loading: false,
             idioms: newProps.searchResults,
         })
-<<<<<<< HEAD
- 
-     };
- 
- render(){
-     const{initView,loading,idioms,errorMasg} = this.state
-     const {searchIdiom} = this.props
- 
-     if (initView){
-         return <h3>Enter an idiom and look for the explanation...{searchIdiom}</h3>
-     }else if (loading){
-         return <h3>loading...</h3>
-     }else if(errorMasg){
-         return <h3>{errorMasg}</h3>
-     }else{
-         return (           
-             <div className="SearchResult">
-                 {
-                     idioms.map((idioms,index) => (
-                     <div className="card rearchCard col-6" key={index}>
-                     <div className="card-body" data-id={idioms._id}>
-                       <h5 className="card-title">{idioms.idiom} </h5>
-                       <p className="card-text">{idioms.meaning}</p>
-                       <LanguageButton />
-                       <button href="#" className="btn bg-warning text-dark">Go Translate !</button>
-                       <LikeButton />
-                       <TranslatedDescription />
-                     </div>
-                   </div> 
-                   ) )
-                     }            
-          </div>
-     
-                 );
-         }
-     };
- }       
-  
-=======
 
     };
 
@@ -200,4 +133,3 @@ export default class SearchResult extends Component {
         }
     };
 }
->>>>>>> 05ed957e50f6e081d43011ecb3d32a59eb06b5ad
