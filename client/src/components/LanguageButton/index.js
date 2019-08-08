2f3547c1001a6b07e5a990ba67f0a92d208333ca
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-08-07 21:18:51 -0400

    translate display working

--- client/src/components/LanguageButton/index.js
+++ client/src/components/LanguageButton/index.js
@@ -1,103 +1,103 @@
-import React, { Component } from 'react';
-import "./Language.css";
-import API from "../../utils/languagesAPI";
-// import propTypes from 'prop-types';
-const request = require('request');
-const uuidv4 = require('uuid/v4');
-const subscriptionKey = "aa4b796d586a40bc9e33ba503ea1d3c4";
-if (!subscriptionKey) {
-    throw new Error('Environment variable for your subscription key is not set.')
-};
+// import React, { Component } from 'react';
+// import "./Language.css";
+// import API from "../../utils/languagesAPI";
+// // import propTypes from 'prop-types';
+// const request = require('request');
+// const uuidv4 = require('uuid/v4');
+// const subscriptionKey = "aa4b796d586a40bc9e33ba503ea1d3c4";
+// if (!subscriptionKey) {
+//     throw new Error('Environment variable for your subscription key is not set.')
+// };
 
-class LanguageButton extends Component {
+// class LanguageButton extends Component {
 
-    state = {
-        buttonLanguagesArray: [],
-        languageCode: "",
-        idioms: null
-    }
+//     state = {
+//         buttonLanguagesArray: [],
+//         languageCode: "",
+//         idioms: null
+//     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            buttonLanguagesArray: [],
-            languageCode: "",
-        };
-        this.handleChange = this.handleChange.bind(this)
-    }
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             buttonLanguagesArray: [],
+//             languageCode: "",
+//         };
+//         this.handleChange = this.handleChange.bind(this)
+//     }
 
 
-    componentWillReceiveProps(newProps) {
-        console.log(newProps);
-        //set loading...
-        this.setState({
-            idioms: newProps.searchResults,  
-        })
+//     componentWillReceiveProps(newProps) {
+//         console.log(newProps);
+//         //set loading...
+//         this.setState({
+//             idioms: newProps.searchResults,  
+//         })
 
-    }   
+//     }   
     
-    componentDidMount() {
-        API.getLanguagesAll()
-            .then(res => {
-                const buttonLanguagesArray = res.data;
-                this.setState({ buttonLanguagesArray });
-                console.log(buttonLanguagesArray);
-            })
-            .catch(err => { console.log("error:", err) })
-    }
+//     componentDidMount() {
+//         API.getLanguagesAll()
+//             .then(res => {
+//                 const buttonLanguagesArray = res.data;
+//                 this.setState({ buttonLanguagesArray });
+//                 console.log(buttonLanguagesArray);
+//             })
+//             .catch(err => { console.log("error:", err) })
+//     }
 
-    handleChange = event => {
-        this.setState({
-            languageCode: event.target.value
-        });
-        console.log(event.target.value);
-        this.translateText(event.target.value); //need to pass the selected idiom to be translated
-    };
+//     handleChange = event => {
+//         this.setState({
+//             languageCode: event.target.value
+//         });
+//         console.log(event.target.value);
+//         this.translateText(event.target.value); //need to pass the selected idiom to be translated
+//     };
 
-    translateText = (l, i) => {    // need to pass the selected idiom to be translated
+//     translateText = (l, i) => {    // need to pass the selected idiom to be translated
 
-        let options = {
-            method: 'POST',
-            baseUrl: 'https://api.cognitive.microsofttranslator.com/',
-            url: 'translate',
-            qs: {
-                'api-version': '3.0',
-                'to': [l]
-            },
-            headers: {
-                'Ocp-Apim-Subscription-Key': subscriptionKey,
-                'Content-type': 'application/json',
-                'X-ClientTraceId': uuidv4().toString()
-            },
-            body: [{
-                'text': i
-            }],
-            json: true,
-        };
+//         let options = {
+//             method: 'POST',
+//             baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+//             url: 'translate',
+//             qs: {
+//                 'api-version': '3.0',
+//                 'to': [l]
+//             },
+//             headers: {
+//                 'Ocp-Apim-Subscription-Key': subscriptionKey,
+//                 'Content-type': 'application/json',
+//                 'X-ClientTraceId': uuidv4().toString()
+//             },
+//             body: [{
+//                 'text': i
+//             }],
+//             json: true,
+//         };
 
-        request(options, function (err, res, body) {
-            console.log(JSON.stringify(body, null, 4));
-        });
-    }
+//         request(options, function (err, res, body) {
+//             console.log(JSON.stringify(body, null, 4));
+//         });
+//     }
 
-    render() {
-        const { buttonLanguagesArray } = this.state;
+//     render() {
+//         const { buttonLanguagesArray } = this.state;
 
-        let buttonLanguagesArrayList = buttonLanguagesArray.length >= 0
-            && buttonLanguagesArray.map((item, i) => {
-                return (
-                    <option key={i} value={item.languageCode}>{item.language}</option>
-                )
-            }, this);
+//         let buttonLanguagesArrayList = buttonLanguagesArray.length >= 0
+//             && buttonLanguagesArray.map((item, i) => {
+//                 return (
+//                     <option key={i} value={item.languageCode}>{item.language}</option>
+//                 )
+//             }, this);
 
-        return (
-            <div>
-                <select onChange={this.handleChange}>
-                    {buttonLanguagesArrayList}
-                </select>
-            </div>
-        );
-    }
-}
+//         return (
+//             <div>
+//                 <select onChange={this.handleChange}>
+//                     {buttonLanguagesArrayList}
+//                 </select>
+//             </div>
+//         );
+//     }
+// }
 
-export default LanguageButton;
+// export default LanguageButton;
--- client/src/components/Searchresult/index.js
+++ client/src/components/Searchresult/index.js
@@ -6,6 +6,7 @@ import "./Searchresult.css"
 import LikeButton from "../LikeButton"
 //  import TranslatedDescription from "../TranslatedDescription"
 import API from "../../utils/languagesAPI";
+// import axios from "axios";
 const request = require('request');
 const uuidv4 = require('uuid/v4');
 const subscriptionKey = "aa4b796d586a40bc9e33ba503ea1d3c4";
@@ -26,6 +27,7 @@ export default class SearchResult extends Component {
         errorMasg: null,
         buttonLanguagesArray: [],
         languageCode: "",
+        translatedText: []
     };
 
     //call back when the component receive a new idiom
@@ -39,7 +41,6 @@ export default class SearchResult extends Component {
             loading: false,
             idioms: newProps.searchResults,
         })
-
     };
 
     componentDidMount() {
@@ -53,18 +54,15 @@ export default class SearchResult extends Component {
     };
 
 
-    handleChange = (event)  => {
+    handleChange = (event) => {
         console.log(this.state.idioms);
         console.log(event.target.value);
-        this.translateText(event.target.value, this.state.idioms[0].meaning) 
-
-        // this.setState({
-        //     languageCode: event.target.value,
-        // });
-      //need to pass the selected idiom to be translated
+        this.translateText(event.target.value, this.state.idioms[0].meaning)
     };
 
     translateText = (l, i) => {    // need to pass the selected idiom to be translated (i) - (l) = the chosen language
+        // const thisthing = this.state.translatedText
+        let translatedText = '';
         let options = {
             method: 'POST',
             baseUrl: 'https://api.cognitive.microsofttranslator.com/',
@@ -83,13 +81,49 @@ export default class SearchResult extends Component {
             }],
             json: true,
         };
+
+        const setText = (text) => {
+            console.log("setText function")
+            this.setState({translatedText: text})
+            console.log("this works")
+        };
+
         request(options, function (err, res, body) {
-            console.log(JSON.stringify(body, null, 4));
-        });
+            console.log(JSON.stringify((body[0].translations[0].text), null, 4));
+            console.log(res.body[0].translations[0].text);
+            const renderedText = res.body[0].translations[0].text;
+            console.log(renderedText);
+            console.log(res);
+            setText(renderedText)
+        })
+        // body[""0""].translations[""0""]
+        // this.setState({translatedText: renderedText})
+
+
+        // const url = `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to%5B0%5D=${l}`
+        // axios.get(url, {
+        //     headers: {
+        //         'Ocp-Apim-Subscription-Key': subscriptionKey,
+        //         'Content-type': 'application/json',
+        //         'X-ClientTraceId': uuidv4().toString()
+        //     },
+        //     body: [{
+        //         'text': i
+        //     }]
+        // })
+        //     .then(res => {
+        //         console.log(res);
+        //         const renderedText = res.body[0].translations[0].text;
+        //         this.setState({ translatedText: renderedText })
+        //     })
+        // "af"
+        // .then(res => {
+        //    this.setState({thisthing: res.body[0].translations[0].text})})
+
     };
 
     render() {
-        const { initView, loading, idioms, errorMasg, buttonLanguagesArray } = this.state
+        const { initView, loading, idioms, errorMasg, buttonLanguagesArray, translatedText } = this.state
         const { searchIdiom } = this.props
 
         let buttonLanguagesArrayList = buttonLanguagesArray.length >= 0
@@ -107,7 +141,9 @@ export default class SearchResult extends Component {
             return <h3>{errorMasg}</h3>
         } else {
             return (
-                <div className="SearchResult">
+                <div className="row d-flex">
+                    <div className="col-lg-12">
+                <div className="SearchResult ml5">
                     {
                         idioms.map((idioms, index) => (
                             <div className="card rearchCard col-6" key={index}>
@@ -117,17 +153,20 @@ export default class SearchResult extends Component {
                                     <br></br>
                                     <div>
                                         <select onChange={this.handleChange}>
+                                            {/* <option value="es"> Spanish </option> */}
                                             {buttonLanguagesArrayList}
                                         </select>
                                     </div>
                                     <br></br>
-                                    <h5 className="card-title">Placeholder - translation </h5>
-                                    <p className="card-text">Placeholder - translation</p>
+                                    <h5 className="card-title">{translatedText} </h5>
+                                    {/* <p className="card-text">Placeholder - translation</p> */}
                                     <LikeButton />
                                 </div>
                             </div>
                         ))
                     }
+                </div>
+                </div>
                 </div>
             );
         }
